9f350ce831a4fa83e409bb60822e073d7f094d49
Lassi Pasanen <spede100@gmail.com>
2019-04-21 19:22:18 +0300

    Kaikenlaista lisäilty

--- src/Melodia.java
+++ src/Melodia.java
@@ -4,10 +4,18 @@ public class Melodia {
 	Apu apuri = new Apu();
 	
 	public void luoMelodiaA() {
+		ArrayList<Motiivi> melodiaA = new ArrayList<Motiivi>();
+		
 		//Arpoo A:n rakenteen: aa(1), ab(2), a + pitka(3)
 		int rakenne = apuri.arpoja(new int[] {1, 1, 2, 1, 3, 1});
 		if (rakenne == 1) {
 			//Motiivilista1 + Motiivilista1
+			ArrayList<Motiivi> a = luoMotiiviLista();
+			for (int j = 0; j<2; j++) {
+				for (int i = 0; i<a.size(); i++) {
+					melodiaA.add(a.get(i));
+				}
+			}
 		}
 		else if (rakenne == 2) {
 			//Motiivilista1 + Motiivilista2
